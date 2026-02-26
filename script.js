bbe47ce2dcec78051a87857b6b0db3bd3c36b75d
HennyMoneyAfricPortals <hennymoneyafric@outlook.com>
2026-02-26 01:06:29 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Register service worker for PWA
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log("Service Worker Registered"))
+    .catch(err => console.log("Service Worker failed:", err));
+}
